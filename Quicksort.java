13868c6b01f0ea24af39469271ddaa671b723992
Zack Zatkin-Gold <zzatkin@gmail.com>
2015-09-27 15:01:59 -0400

    add quicksort

---
+++ Quicksort.java
@@ -0,0 +1,40 @@
+/**
+ * This is a very naive implementation of quicksort. For a much faster
+ * implementation, refer to java.util.DualPivotQuicksort.
+ */
+public class Quicksort {
+    private int[] a;
+
+    private class qsort {
+        private void sort(int left, int right) {
+            int i = left;
+            int j = right;
+            int pivot = a[left + (right - left) / 2];
+            while(i <= j) {
+                while(a[i] < pivot) i++;
+                while(a[j] > pivot) j--;
+                if(i <= j) {
+                    int temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+            if(left < j) sort(left, j);
+            if(i < right) sort(i, right);
+        }
+        public qsort() {
+            if(a == null || a.length == 0) return;
+            sort(0, a.length - 1);
+        }
+    }
+
+    public Quicksort() {
+        a = new int[]{10,4,7,2,8,3,1,6,4,5};
+        qsort sort = new qsort();
+
+        for(int i = 0; i < a.length; i++)
+            System.out.println(a[i]);
+    }
+}
